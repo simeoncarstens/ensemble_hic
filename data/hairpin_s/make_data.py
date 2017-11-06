Generate test data exactly following the statistical model; fixing DOS calculation

data/hairpin_s/make_data.py
@@ -6,14 +6,16 @@ import matplotlib.pyplot as plt
 from csb.bio.utils import distance_matrix
 from csb.statistics.pdf import MultivariateGaussian
 numpy.seterr(all='raise')
+from ensemble_hic import kth_diag_indices
+numpy.random.seed(42)
 
 def zero_diagonals(a, n):
+    b = a.copy()
     for i in range(-n, n+1):
-        inds = kth_diag_indices(a, i)
-        a[inds] = 0
+        inds = kth_diag_indices(b, i)
+        b[inds] = 0
         
-    return a
-
+    return b
 
 if True:
     top = numpy.vstack((numpy.arange(7), numpy.zeros(7), numpy.zeros(7)))
@@ -35,26 +37,48 @@ if False:
     init2 = numpy.vstack((init2, [5, 1, 0], init2[::-1,:] + numpy.array([0,2,0])[None,:], numpy.array([-1, 2, 0])))
 
 ## size of fake ensemble
-ensemble_size = 10000
+ensemble_size = 100
 ## sigma of Gausian noise added to positions to generate random ensemble
-sigma = 0.05
+sigma = 0.01
 ## cutoff from which to determine contact frequencies
 cutoff = 1.8
 
 n_beads = len(init1)
 
-g = MultivariateGaussian(mu=init1.ravel(), sigma=sigma * numpy.eye(n_beads*3))
-ensemble1 = g.random(size=ensemble_size).reshape(ensemble_size, n_beads, 3)
-g = MultivariateGaussian(mu=init2.ravel(), sigma=sigma * numpy.eye(n_beads*3))
-ensemble2 = g.random(size=ensemble_size).reshape(ensemble_size, n_beads, 3)
-dms1 = numpy.array(map(distance_matrix, ensemble1))
-dms2 = numpy.array(map(distance_matrix, ensemble2))
-frequencies1 = numpy.sum(dms1 < cutoff, 0)
-frequencies2 = numpy.sum(dms2 < cutoff, 0)
-summed_frequencies = frequencies1 + frequencies2
 
-from misc import kth_diag_indices
 if True:
+    from ensemble_hic.forward_models import EnsembleContactsFWM
+
+    suffix = '_fwm_poisson'
+    data_points = array([[i, j, 0] for i in range(n_beads) for j in xrange(i+1, n_beads)])
+    fwm = EnsembleContactsFWM('asdfasdf', 2, np.ones(len(data_points)) * 2.3, data_points)
+    md = fwm(norm=1.0, smooth_steepness=10,
+             structures=concatenate((init1,init2)).ravel(), weights=np.ones(2))
+    temp = np.random.poisson(ensemble_size * md)
+    summed_frequencies = np.zeros((n_beads, n_beads))
+    summed_frequencies[data_points[:,0], data_points[:,1]] = temp
+    summed_frequencies[data_points[:,1], data_points[:,0]] = temp
+    summed_frequencies = summed_frequencies.astype(int)
+    
+if False:
+    if sigma == 0.01:
+        suffix = '_littlenoise'
+    elif sigma == 0.05:
+        suffix = ''
+    else:
+        raise
+    g = MultivariateGaussian(mu=init1.ravel(), sigma=sigma * numpy.eye(n_beads*3))
+    ensemble1 = g.random(size=ensemble_size).reshape(ensemble_size, n_beads, 3)
+    g = MultivariateGaussian(mu=init2.ravel(), sigma=sigma * numpy.eye(n_beads*3))
+    ensemble2 = g.random(size=ensemble_size).reshape(ensemble_size, n_beads, 3)
+    dms1 = numpy.array(map(distance_matrix, ensemble1))
+    dms2 = numpy.array(map(distance_matrix, ensemble2))
+    frequencies1 = numpy.sum(dms1 < cutoff, 0)
+    frequencies2 = numpy.sum(dms2 < cutoff, 0)
+    summed_frequencies = frequencies1 + frequencies2
+
+#from misc import kth_diag_indices
+if not True:
     fig = plt.figure()
     ax1 = fig.add_subplot(131)
     frequencies1 = zero_diagonals(frequencies1, 2)
@@ -78,7 +102,7 @@ if True:
     fig.colorbar(ms, ax=ax3)
 
 if True:
-    with open(os.path.expanduser('~/projects/hic/py/hicisd2/ensemble_scripts/toy_test/spiral_hairpin_data_{}beads_littlenoise.txt'.format(n_beads)), 'w') as opf:
+    with open(os.path.expanduser('~/projects/ensemble_hic/data/hairpin_s/hairpin_s{}.txt'.format(suffix)), 'w') as opf:
         for i in range(n_beads):
             for j in range(i + 1, n_beads):
                 opf.write('{}\t{}\t{}\n'.format(i, j, summed_frequencies[i,j]))

data/proteins/make_data.py
@@ -3,7 +3,7 @@ Reconstruction of GB1 and Sh3 from mixed contacts
 """
 import os, sys, numpy as np, glob
 
-pypath = os.path.expanduser('~/projects/hic/py/hicisd2/ensemble_scripts/protein_test/')
+pypath = os.path.expanduser('~/projects/ensemble_hic/data/proteins/')
 os.chdir(pypath)
 if not pypath in sys.path: sys.path.insert(0, pypath)
 
@@ -16,7 +16,7 @@ from scipy.spatial.distance import squareform
 from isd import utils
 from isd.Distance import Distance
 from isd.DataSet import DataSet
-
+from ensemble_hic import kth_diag_indices
 
 def zero_diagonals(a, n):
     for i in range(-n, n+1):
@@ -49,9 +49,9 @@ coords2 = StructureParser(prot2 + '.pdb').parse().get_coordinates(['CA']) / 4.0
 
 
 ## size of fake ensemble
-ensemble_size = 1000
+ensemble_size = 100
 ## sigma of Gausian noise added to positions to generate random ensemble
-sigma = 0.05
+sigma = 0.01
 ## cutoff from which to determine contact frequencies
 cutoff = 1.8
 
@@ -68,16 +68,14 @@ if False:
     frequencies1 = numpy.sum(dms1 < cutoff, 0)
     frequencies2 = numpy.sum(dms2 < cutoff, 0)
     summed_frequencies = frequencies1 + frequencies2
-
-if False:
+elif False:
     ## create Poisson-distributed data
     dm1 = distance_matrix(coords)
     dm2 = distance_matrix(coords2)
     cs1 = ensemble_size * (dm1 < cutoff)
     cs2 = ensemble_size * (dm2 < cutoff)
     summed_frequencies = numpy.random.poisson(cs1 + cs2)
-
-if True:
+elif False:
     ## create Poisson-distributed data based on probabilites obtained form
     ## non-central Maxwell distribution
     sys.path.append(os.path.expanduser('~/projects/hic/py/'))
@@ -100,9 +98,25 @@ if True:
         summed_frequencies = numpy.random.poisson(ensemble_size * probs1)      
     else:
         summed_frequencies = numpy.random.poisson(ensemble_size * (probs1 + probs2))
-    
+elif True:    
+    from ensemble_hic.forward_models import EnsembleContactsFWM
 
-from misc import kth_diag_indices
+    suffix = 'fwm_poisson'
+    n_structures = 1 if prot1 == prot2 else 2
+    data_points = array([[i, j, 0] for i in range(n_beads)
+                         for j in xrange(i+1, n_beads)])
+    fwm = EnsembleContactsFWM('asdfasdf', n_structures,
+                              np.ones(len(data_points)) * 2, data_points)
+    structures = np.concatenate((coords,coords2)) if n_structures == 2 else coords
+    md = fwm(norm=1.0, smooth_steepness=10,
+             structures=structures.ravel(), weights=np.ones(n_structures))
+    temp = np.random.poisson(ensemble_size * md)
+    summed_frequencies = np.zeros((n_beads, n_beads))
+    summed_frequencies[data_points[:,0], data_points[:,1]] = temp
+    summed_frequencies[data_points[:,1], data_points[:,0]] = temp
+    summed_frequencies = summed_frequencies.astype(int)
+
+# from misc import kth_diag_indices
 if False:
     fig = plt.figure()
     ax1 = fig.add_subplot(131)
@@ -129,11 +143,7 @@ if False:
 if True:
     if prot1 == prot2:
         prot2 = 'none'
-    if ensemble_size == 100:
-        fname = '{}_{}_maxwell_poisson_data_es100_sigma0.05.txt'.format(prot1, prot2)
-    elif ensemble_size == 1000:
-        fname = '{}_{}_maxwell_poisson_data_sigma0.05.txt'.format(prot1, prot2)
-    with open(os.path.expanduser('~/projects/ensemble_hic/data/proteins/{}'.format(fname)), 'w') as opf:
+    with open(os.path.expanduser('~/projects/ensemble_hic/data/proteins/{}_{}/{}.txt'.format(prot1, prot2, suffix)), 'w') as opf:
         for i in range(n_beads):
             for j in range(i + 1, n_beads):
                 opf.write('{}\t{}\t{}\n'.format(i, j, summed_frequencies[i,j]))

ensemble_hic/setup_functions.py
@@ -180,7 +180,7 @@ def make_replica_schedule(replica_params, n_replicas):
         if separate_prior_annealing:
             b_chain = np.arange(0, np.floor(n_replicas / 2))
             l_chain = np.arange(np.floor(n_replicas / 2), n_replicas)
-            lambdas = np.concatenate((np.zeros(len(beta_chain)) + l_min,
+            lambdas = np.concatenate((np.zeros(len(b_chain)) + l_min,
                                       np.linspace(l_min, l_max,
                                                   len(l_chain))))
             betas = np.concatenate((np.linspace(b_min, b_max,

scripts/misc/calculate_DOS.py
@@ -13,15 +13,14 @@ from ensemble_hic.setup_functions import parse_config_file, make_posterior
 from ensemble_hic.setup_functions import setup_weights
 from ensemble_hic.analysis_functions import load_samples
 
-# config_file = '/scratch/scarste/ensemble_hic/bau2011/GM12878_10structures_s_106replicas_nosphere_optsched3/config.cfg'
-config_file = '/scratch/scarste/ensemble_hic/bau2011/GM12878_10structures_s_123replicas_nosphere_specificoptsched/config.cfg'
-config_file = '/scratch/scarste/ensemble_hic/bau2011/K562_10structures_s_106replicas_nosphere_optsched3/config.cfg'
-config_file = '/scratch/scarste/ensemble_hic/hairpin_s/hairpin_s_littlenoise_radius10_2structures_sn_20replicas/config.cfg'
 config_file = sys.argv[1]
+
 settings = parse_config_file(config_file)
 n_replicas = int(settings['replica']['n_replicas'])
 target_replica = n_replicas
-# params = {'burnin': 5000,
+
+# params = {'n_samples': 100001,
+#           'burnin': 80001,
 #           'samples_step': 10,
 #           'niter': int(1e6),
 #           'tol': 1e-10
@@ -80,7 +79,13 @@ wham = WHAM(len(energies_flat), n_replicas)
 wham.N[:] = len(energies_flat)/n_replicas
 wham.run(q, niter=params['niter'], tol=params['tol'], verbose=100)
 
-dos = DOS(energies_flat.sum(1), wham.s, sort_energies=False)
+from csb.numeric import log_sum_exp
+dos_2d = DOS(energies_flat, wham.s, sort_energies=False)
+#print dos_2d.log_Z(np.array([1.,1.])) - dos_2d.log_Z(np.array([0.,1.]))
+print log_sum_exp(-dos_2d.E.sum(1) + dos_2d.s) - log_sum_exp(-dos_2d.E[:,1] + dos_2d.s)
+
+
+dos = DOS(energies_flat, wham.s, sort_energies=False)
 
 ana_path = output_folder + 'analysis/'
 if not os.path.exists(ana_path):
@@ -91,36 +96,12 @@ with open(ana_path + 'wham_params.pickle', 'w') as opf:
     dump(params, opf)
 
 if False:
-    import sys, os
-    pypath = os.path.expanduser('~/projects/adarex/py')
-    if not pypath in sys.path: sys.path.insert(0, pypath)
-    from scheduler import Scheduler, RelativeEntropy, SwapRate, SimpleScheduler, SwapRate
-    from csbplus.statmech.ensembles import BoltzmannEnsemble
+    from csb.numeric import log_sum_exp
+    from csb.statistics.rand import sample_from_histogram
+    logp = lambda beta: dos.s - beta * dos.E - log_sum_exp(dos.s - beta * dos.E)
+    p = lambda beta: np.exp(logp(beta))
+
+    betas = np.load(output_folder + 'analysis/interp_dos_sched.pickle')['beta']
+    states = [samples[np.unravel_index(sample_from_histogram(p(beta)), samples.shape)] for beta in betas]
     from cPickle import dump
-    
-    ensemble = BoltzmannEnsemble(dos=dos)
-    entropy  = Scheduler(ensemble, RelativeEntropy(), np.greater)
-    
-    target = 2.0
-
-    entropy.find_schedule(target, 0., 1., verbose=True)
-    
-    beta = np.array(entropy.schedule)
-    beta[0] = 0.
-    beta[-1] = 1.
-
-    step = 1
-    tempsched = SimpleScheduler(ensemble, SwapRate(), comparison=np.less)
-    pred_swap_rates = [tempsched.eval_criterion(beta[i], beta[i+1])
-                       for i in range(len(beta)-1)[::step]]
-
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot((beta[1:] + beta[:-1])[::step] / 2.0, pred_swap_rates)
-    ax.set_xlabel('beta')
-    ax.set_ylabel('predicted swap rate')
-    plt.show()
-
-    with open('/usr/users/scarste/schedules/K562_75replicas.pickle','w') as opf:
-        dump(dict(lammda=beta, beta=beta), opf)
+    dump(states, open('/scratch/scarste/ensemble_hic/hairpin_s/initstates.pickle','w'))

scripts/misc/calculate_optimal_schedule.py
@@ -13,12 +13,14 @@ target_entropy = float(sys.argv[2])
 variables = sys.argv[3]
 output_file = sys.argv[4]
 
-ensemble = BoltzmannEnsemble(dos=dos)
+from csbplus.statmech.dos import DOS
+#ensemble = BoltzmannEnsemble(dos=dos)
+ensemble = BoltzmannEnsemble(dos=DOS(dos.E.sum(1), dos.s))
 entropy  = Scheduler(ensemble, RelativeEntropy(), np.greater)
-entropy.find_schedule(target_entropy, 0., 1., verbose=True)
+entropy.find_schedule(target_entropy, 1e-6, 1., verbose=True)
 
 beta = np.array(entropy.schedule)
-beta[0] = 0.
+beta[0] = 1e-6
 beta[-1] = 1.
 
 if False:

scripts/plots/compare_with_known.py
@@ -9,9 +9,10 @@ from scipy.cluster.vq import kmeans2
 from ensemble_hic.setup_functions import parse_config_file
 from ensemble_hic.analysis_functions import load_sr_samples
 
-if True:
+if not True:
     config_file = sys.argv[1]
 else:
+    # config_file = '/scratch/scarste/ensemble_hic/proteins/1pga_1shf_fwm_poisson_10structures_sn_80replicas/config.cfg'
     pass
 data_dir = os.path.expanduser('~/projects/ensemble_hic/data/proteins/')
 config = parse_config_file(config_file)
@@ -19,7 +20,7 @@ config = parse_config_file(config_file)
 n_structures = int(config['general']['n_structures'])
 n_replicas = 80
 n_samples = int(config['replica']['n_samples']) + 1
-burnin = 10000
+burnin = 30000
 save_figures = True
 
 data_file = config['general']['data_file']
@@ -55,7 +56,7 @@ if True:
         ax = fig.add_subplot(len(knowns),1,i+1)
         ax.hist(rmsds[i], bins=np.linspace(0.0,6,np.sqrt(len(ens_flat))))
         ax.set_xlabel('RMSD to ' + fnames[i])
-        ax.set_xlim((0.3, 4.0))
+        #ax.set_xlim((0.3, 4.0))
     
     fig.tight_layout()
     if save_figures: