98345d390a1b5fda72245ad403576a41edbe3119
simeon_carstens <devnull@localhost>
2019-11-11 16:52:32 +0100

    Changed evidence plot script to use precalculated data

--- scripts/plots/evidences.py
+++ scripts/plots/evidences.py
@@ -2,98 +2,35 @@ import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
-from scipy.special import gammaln
-from ensemble_hic.setup_functions import make_posterior, parse_config_file
-from ensemble_hic.analysis_functions import load_sr_samples
-
-from csb.numeric import log_sum_exp
-sys.path.append(os.path.expanduser('~/projects/ensemble_hic/scripts/misc/'))
-from simlist import simulations
-
-# which = ('1pga_1shf_fwm_poisson_new_it3',
-#          )
-# which = ('1pga_1shf_fwm_poisson_new_fixed_it3',
-#          )
-which = ('nora2012_15kbbins_fixed',)
 
 
-n_structures = simulations[which[0]]['n_structures']
-all_logZs = []
-for sim in which:
-    current = simulations[sim]
-    output_dirs = [current['common_path'] + x for x in current['output_dirs']]
-    n_structures = current['n_structures']
-    
-    logZs = []
-    data_terms = []
-    for x in output_dirs:
-        dos = np.load(x + '/analysis/dos.pickle')
-        logZs.append(log_sum_exp(-dos.E.sum(1) + dos.s) - \
-                     log_sum_exp(-dos.E[:,1] + dos.s))
-        a = x.find('replicas')
-        b = x[a-4:].find('_')
-        n_replicas = int(x[a-4+b+1:a])
-
-        p = np.load(x + '/analysis/wham_params.pickle')
-        c = parse_config_file(x + '/config.cfg')
-        s = load_sr_samples(x + '/samples/', n_replicas, p['n_samples']+1,
-                            int(c['replica']['samples_dump_interval']),
-                            p['burnin'])
-        sels = np.load(x + '/analysis/wham_sels.pickle')
-        s = s[sels[-1]]
-        p = make_posterior(parse_config_file(x + '/config.cfg'))
-        L = p.likelihoods['ensemble_contacts']
-        d = L.forward_model.data_points[:,2]
-        f = gammaln(d+1).sum()
-        print "mean log-posterior:", np.mean(map(lambda x: p.log_prob(**x.variables), s))
-        logZs[-1] -= f + np.log(len(d)) * (not '1pga' in x)
-        data_terms.append(np.array(map(lambda x: -L.log_prob(**x.variables), s)).mean() + f)
-        print "evidence:", logZs[-1]
-    data_terms = np.array(data_terms)    
-    
-    all_logZs.append(logZs)
-
-all_logZs = np.array(all_logZs)
-
-
-def plot_evidences(ax):
+def plot_evidences(ax, plot_data_file):
     def unique(array):
         uniq, index = np.unique(array, return_index=True)
         return uniq[index.argsort()]
-        
-    n_structures = simulations[which[0]]['n_structures']
-        
-    mean_logZs = np.array([all_logZs[0, np.array(n_structures) == i].mean()
-                           for i in n_structures])
-    mean_logZs = unique(mean_logZs)
-    mean_logZs = all_logZs[0,:]
-    mean_data_terms = np.array([data_terms[np.array(n_structures) == i].mean()
-                                for i in n_structures])
-    mean_data_terms = unique(mean_data_terms)
-    mean_data_terms = data_terms
-    #n_structures = unique(n_structures)
-    ax.plot(n_structures, mean_logZs, ls='--', marker='o', label='evidence',
-            color='black')
-    ax2 = ax.twinx()
     
-    ax2.plot(n_structures, mean_data_terms, ls='--', marker='s',
-             label='data energy', color='gray')
+    n_structures, logZs, data_terms = np.load(plot_data_file)
+    ax.plot(n_structures, logZs, ls='--', marker='o', label='evidence',
+            color='black')
     ax.set_ylabel('log(evidence) / # data points', color='black', fontsize=14)
-    ax2.set_ylabel(r'$-\langle$log $L \rangle$', color='gray', fontsize=14)
     ax.set_xlabel('number of states $n$', fontsize=14)
-    ax2.set_xticks(())
     ax.set_xticks(n_structures)
     ax.spines['top'].set_visible(False)
-    ax2.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    ax.ticklabel_format(style='sci', scilimits=(0,0),
-                        axis='y')
-    ax2.ticklabel_format(style='sci', scilimits=(0,0),
-                         axis='y')
+    ax.ticklabel_format(style='sci', scilimits=(0,0), axis='y')
+    
+    ax2 = ax.twinx()
+    ax2.plot(n_structures, data_terms, ls='--', marker='s',
+             label='data energy', color='gray')
+    ax2.set_ylabel(r'$-\langle$log $L \rangle$', color='gray', fontsize=14)
+    ax2.set_xticks(())
+    ax2.spines['top'].set_visible(False)
+    ax2.ticklabel_format(style='sci', scilimits=(0,0), axis='y')
+
     ## make inset
     from mpl_toolkits.axes_grid1.inset_locator import inset_axes
     inset_ax = inset_axes(ax, width='40%', height='25%', loc=10)
-    inset_ax.plot((10, 15, 20, 25, 30, 35, 35, 40, 100), mean_logZs[2:], ls='--', marker='o',
+    inset_ax.plot(n_structures[2:], logZs[2:], ls='--', marker='o',
                   label='evidence', color='black')
     inset_ax.set_xticks((10, 20, 30, 40))
     inset_ax.spines['top'].set_visible(False)
@@ -101,16 +38,6 @@ def plot_evidences(ax):
     inset_ax.set_ylim((-7600, -7300))
     inset_ax.set_xlim((11, 42))
     
-
-if False:
-    fig, ax = plt.subplots()
-    plot_evidences(ax)
-    fig.tight_layout()
-    plt.show()
-    if not not False:
-        fig.set_size_inches((5.8, 2.4))
-        fig.tight_layout()
-
 
 
 def plot_evidences_proteins():
@@ -202,3 +129,64 @@ def plot_evidences_proteins():
         fig, (top_ax, bottom_ax) = plt.subplots(2, 1, sharex=True)
         make_bothaxes(top_ax, bottom_ax, fig)
         fig.tight_layout()
+
+
+if __name__ == "__main__":
+
+    ## for now only for 5C simulations
+    
+    import os, sys
+    import numpy as np
+    from scipy.special import gammaln
+    from ensemble_hic.setup_functions import make_posterior, parse_config_file
+    from ensemble_hic.analysis_functions import load_sr_samples
+    from csb.numeric import log_sum_exp
+    from cPickle import dump
+    sys.path.append(os.path.expanduser('~/projects/ensemble_hic/scripts/misc/'))
+
+    out_file = sys.argv[1]
+    
+    common_path = '/scratch/scarste/ensemble_hic/nora2012/15kbbins_bothdomains_fixed_'
+    simulations = ((1, 'it4_1structures_82replicas'),
+                   (5, 'it4_5structures_159replicas'),
+                   (10, 'it4_10structures_150replicas'),
+                   (15, 'it4_15structures_148replicas'),
+                   (20, 'it4_20structures_148replicas'),
+                   (25, 'it4_25structures_172replicas'),
+                   (30, 'it4_30structures_172replicas'),
+                   (35, 'it4_35structures_172replicas'),
+                   (40, 'it4_40structures_172replicas'),
+                   (100,'it3_100structures_186replicas'),
+                   )
+    n_structures = [x[0] for x in simulations]
+    output_dirs = [common_path + x[1] for x in simulations]
+    
+    logZs = []
+    data_terms = []
+    for x in output_dirs:
+        dos = np.load(x + '/analysis/dos.pickle')
+        logZs.append(log_sum_exp(-dos.E.sum(1) + dos.s) - \
+                     log_sum_exp(-dos.E[:,1] + dos.s))
+        a = x.find('replicas')
+        b = x[a-4:].find('_')
+        n_replicas = int(x[a-4+b+1:a])
+        
+        p = np.load(x + '/analysis/wham_params.pickle')
+        c = parse_config_file(x + '/config.cfg')
+        s = load_sr_samples(x + '/samples/', n_replicas, p['n_samples']+1,
+                            int(c['replica']['samples_dump_interval']),
+                            p['burnin'])
+        sels = np.load(x + '/analysis/wham_sels.pickle')
+        s = s[sels[-1]]
+        p = make_posterior(parse_config_file(x + '/config.cfg'))
+        L = p.likelihoods['ensemble_contacts']
+        d = L.forward_model.data_points[:,2]
+        f = gammaln(d+1).sum()
+        print "mean log-posterior:", np.mean(map(lambda x: p.log_prob(**x.variables), s))
+        logZs[-1] -= f + np.log(len(d)) * (not '1pga' in x)
+        data_terms.append(np.array(map(lambda x: -L.log_prob(**x.variables), s)).mean() + f)
+        print "evidence:", logZs[-1]
+    data_terms = np.array(data_terms)    
+
+    with open(out_file, "w") as opf:
+        dump((n_structures, logZs, data_terms), opf)
