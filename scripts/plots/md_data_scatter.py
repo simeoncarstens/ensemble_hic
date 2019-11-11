75370cebb85da1eae061a062db2f916589f62440
simeon_carstens <devnull@localhost>
2019-11-11 15:43:49 +0100

    Changed mock data / data scatter script to use precalculated values for plotting

--- scripts/plots/md_data_scatter.py
+++ scripts/plots/md_data_scatter.py
@@ -6,56 +6,28 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-from ensemble_hic.setup_functions import parse_config_file, make_posterior
-from ensemble_hic.analysis_functions import load_sr_samples, load_samples_from_cfg_auto
-
-simlist = ((1, 298, 50001, 30000,  '_it3', '', 1000),
-           (5, 218, 50001, 30000,  '_it3', '', 1000),
-           (10, 297, 50001, 30000, '', '', 500),
-           (20, 309, 50001, 30000, '_fixed_it3', '_rep1', 1000),
-           (20, 309, 50001, 30000, '_fixed_it3', '_rep2', 1000),
-           (20, 309, 50001, 30000, '_fixed_it3', '_rep3', 1000),
-           (30, 330, 32001, 20000, '', '_rep1', 1000),
-           (30, 330, 43001, 30000, '', '_rep2', 1000),
-           (30, 330, 43001, 30000, '', '_rep3', 1000),
-           (40, 330, 33001, 20000, '_it2', '', 1000),
-           (40, 330, 33001, 20000, '_it2', '_rep1', 1000),
-           (40, 330, 33001, 20000, '_it2', '_rep2', 1000))        
-
-n_structures, n_replicas, n_samples, burnin, it, rep, di = simlist[5]
 n_beads = 308
+    
 
-sim_path = '/scratch/scarste/ensemble_hic/nora2012/bothdomains{}{}_{}structures_{}replicas/'.format(it, rep, n_structures, n_replicas)
+def plot_md_d_scatter(ax, plot_data_file):
 
-
-config_file = sim_path + 'config.cfg'
-settings = parse_config_file(config_file)
-samples = load_samples_from_cfg_auto(config_file, 30000)
-p = make_posterior(settings)
-fwm = p.likelihoods['ensemble_contacts'].forward_model
-energies = np.array(map(lambda x: -p.log_prob(**x.variables), samples))
-map_sample = samples[np.argmin(energies)]
-n_structures = fwm.n_structures
-
-def plot_md_d_scatter(ax):
+    all_but_sel_x, all_but_sel_y, sels_x, sels_y, maks = np.load(plot_data_file)
+    
+    settings = samplescfg["settings"]
+    samples = samplescfg["samples"]
+    
     ax.set_aspect('equal')
     markers = ('*', 's', 'o')
     cs = ('k', 'gray', 'lightgray')
-    sels = [np.where(fwm.data_points[:,1] - fwm.data_points[:,0] == sep)[0]
-            for sep in (2,3)]
-    antisel = np.array([i for i in np.arange(len(fwm.data_points))
-                        if not i in [x for y in sels for x in y]])
-    ax.scatter(fwm.data_points[antisel,2], fwm(**map_sample.variables)[antisel],
+
+    ax.scatter(all_but_sels_x, all_but_sels_y,
                s=12, marker=markers[-1], c=cs[-1], alpha=0.6, label='$|i-j|>3$')
-    for i, sel in enumerate(sels):
-        ax.scatter(fwm.data_points[sel,2], fwm(**map_sample.variables)[sel],
-                   s=12, marker=markers[i], c=cs[i], alpha=0.6,
+    for i, (sel_x, sel_y) in enumerate(zip(sels_x, sels_y)):
+        ax.scatter(sel_x, sel_y, s=12, marker=markers[i], c=cs[i], alpha=0.6,
                    label='$|i-j|={}$'.format(i+2))
-    maks = np.concatenate((fwm.data_points[:,2], fwm(**map_sample.variables))).max()
     ax.plot((0, maks * 1.1), (0, maks * 1.1), ls='--', c='k')
     ax.set_xscale('log')
     ax.set_yscale('log')
-    print np.corrcoef(fwm.data_points[:,2], fwm(**map_sample.variables))[0,1]
     ax.set_xlim(0.9, maks * 1.1)
     ax.set_ylim(0.9, maks * 1.1)
     ax.set_xlabel('experimental counts', fontsize=14)
@@ -69,3 +41,30 @@ def plot_md_d_scatter(ax):
     ax.legend(handles, labels, frameon=False, title='linear distance [beads]:')
 
     
+if __name__ == "__main__":
+
+    import sys
+
+    cfg_file = sys.argv[1]
+    out_file = sys.argv[2]
+    
+    settings = parse_config_file(cfg_file)
+    samples = load_sr_samples(settings['general']['output_folder'] + 'samples/',
+                              int(settings['replica']['n_replicas']),
+                              50001,
+                              1000,
+                              30000)
+    p = make_posterior(settings)
+    fwm = p.likelihoods['ensemble_contacts'].forward_model
+    energies = np.array(map(lambda x: -p.log_prob(**x.variables), samples))
+    map_sample = samples[np.argmin(energies)]
+    n_structures = fwm.n_structures
+    sels = [np.where(fwm.data_points[:,1] - fwm.data_points[:,0] == sep)[0]
+            for sep in (2,3)]
+    antisel = np.array([i for i in np.arange(len(fwm.data_points))
+                        if not i in [x for y in sels for x in y]])
+    all_but_sel_x = fwm.data_points[antisel,2]
+    all_but_sel_y = fwm(**map_sample.variables)[antisel]
+    sels_x = [fwm.data_points[sel,2] for sel in sels]
+    sels_y = [fwm(**map_sample.variables)[sel] for sel in sels]
+    maks = np.concatenate((fwm.data_points[:,2], fwm(**map_sample.variables))).max()
