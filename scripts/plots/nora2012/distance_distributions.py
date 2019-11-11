98682d00b23b178cf00502be7a392e3ae9e6e2fc
simeon_carstens <devnull@localhost>
2019-11-11 17:30:20 +0100

    Changed distance distributions plot script to use precalculated data

--- scripts/plots/nora2012/distance_distributions.py
+++ scripts/plots/nora2012/distance_distributions.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from ensemble_hic.analysis_functions import load_sr_samples
 
+
 probes = (
     ('pEN1',  100423573, 100433412, 'Linx'),
     ('pEN2',  100622909, 100632521, 'Xite'),
@@ -15,59 +16,56 @@ probes = (
     ('X4',    100557118, 100569724, 'Chic1')
     )
 
-dpath = os.path.expanduser('~/projects/ensemble_hic/data/nora2012/giorgetti2014/DNA_FISH_resume.xlsx')
-from xlrd import open_workbook
-wb = open_workbook(dpath)
-sheet = wb.sheets()[0]
-table = np.array([np.array(sheet.row_values(j))[1:13]
-                  for j in [2,3]+range(7, sheet.nrows)])
-data = {'{}:{}'.format(x[0], x[1]): np.array([float(y) for y in x[2:] if len(y) > 0])
-        for x in table.T}
-
-bead_size = 3000
-region_start = 100378306
-
-simlist = ((1, 298, 50001, 30000,  '_it3', '', 1000),
-           (5, 218, 50001, 30000,  '_it3', '', 1000),
-           (10, 297, 50001, 30000, '', '', 500),
-           (20, 309, 50001, 30000, '_fixed_it3', '_rep1', 1000),
-           (20, 309, 50001, 30000, '_fixed_it3', '_rep2', 1000),
-           (20, 309, 50001, 30000, '_fixed_it3', '_rep3', 1000),
-           (30, 330, 32001, 20000, '', '_rep1', 1000),
-           (30, 330, 43001, 30000, '', '_rep2', 1000),
-           (30, 330, 43001, 30000, '', '_rep3', 1000),
-           (40, 330, 33001, 20000, '_it2', '', 1000),
-           (40, 330, 33001, 20000, '_it2', '_rep1', 1000),
-           (40, 330, 33001, 20000, '_it2', '_rep2', 1000),
-           )
-
-n_structures, n_replicas, n_samples, burnin, it, rep, di = simlist[5]
-n_beads = 308
-
-sim_path = '/scratch/scarste/ensemble_hic/nora2012/bothdomains{}{}_{}structures_{}replicas/'.format(it, rep, n_structures, n_replicas)
-
-samples = load_sr_samples(sim_path + 'samples/', n_replicas,
-                          n_samples, di, burnin)
-X = np.array([s.variables['structures'].reshape(n_structures,-1,3) for s in samples])
-
-Xflat = X.reshape(-1, n_beads, 3) * 53
+## combinations of probes tested
+combinations = ((1,2), (1,6), (1,5), (5,6), (2,1), (0,3), (1,4)) 
     
-get_bead = lambda p: int((np.mean(p[1:3]) - region_start) / bead_size)
 
+def calculate_data(config_file, fish_data_file):
+    
 
-n_bins = int(np.sqrt(len(Xflat)) / 3)
-combs = ((1,2), (1,6), (1,5), (5,6), (2,1), (0,3), (1,4)) 
-mapping = (data['pEN2:pLG1'], data['pEN2:X4'], data['pEN2:X3'], data['X4:X3'],
-           data['pLG1:pEN2'], data['Dxpas34:pEN1'], data['pEN2:pLG11'])
+    from xlrd import open_workbook
+    from ensemble_hic.setup_functions import parse_config_file
+    
+    wb = open_workbook(fish_data_file)
+    sheet = wb.sheets()[0]
+    table = np.array([np.array(sheet.row_values(j))[1:13]
+                      for j in [2,3]+range(7, sheet.nrows)])
+    data = {'{}:{}'.format(x[0], x[1]): np.array([float(y) for y in x[2:] if len(y) > 0])
+            for x in table.T}
 
-def plot_distance_hists(ax, i, l1, l2):
-    ax.hist(np.linalg.norm(Xflat[:,get_bead(probes[l1])] -
-                           Xflat[:,get_bead(probes[l2])],
-                           axis=1),
-            bins=n_bins, histtype='step', label='model',
+    bead_size = 3000
+    region_start = 100378306
+    n_beads = 308
+
+    settings = parse_config_file(config_file)
+    output_folder = settings['general']['output_folder']
+    n_structures = int(settings['general']['n_structures'])
+    samples = load_sr_samples(output_folder + 'samples/',
+                              int(settings['replica']['n_replicas']),
+                              50001, 1000, 30000)
+    X = np.array([s.variables['structures'].reshape(n_structures,-1,3) for s in samples])
+    Xflat = X.reshape(-1, n_beads, 3) * 53
+    
+    get_bead = lambda p: int((np.mean(p[1:3]) - region_start) / bead_size)
+    
+    mapping = (data['pEN2:pLG1'], data['pEN2:X4'], data['pEN2:X3'], data['X4:X3'],
+               data['pLG1:pEN2'], data['Dxpas34:pEN1'], data['pEN2:pLG11'])
+
+    isd_distance_dists = [np.linalg.norm(Xflat[:,get_bead(probes[l1])] -
+                                         Xflat[:,get_bead(probes[l2])],
+                                         axis=1)
+                          for (l1, l2) in combinations]
+    fish_distance_hists = [mapping[i-1] for i in range(len(combinations))]
+
+    return isd_distance_dists, fish_distance_hists
+
+def plot_distance_hists(ax, isd_distances, fish_distances, l1, l2):
+
+    n_bins = int(np.sqrt(len(isd_distances)) / 3)
+    ax.hist(isd_distances, bins=n_bins, histtype='step', label='model',
             normed=True, color='black', lw=1)
-    ax.hist(mapping[i-1],
-            bins=int(np.sqrt(len(mapping[i-1]))), histtype='step',
+    ax.hist(fish_distances,
+            bins=int(np.sqrt(len(fish_distances))), histtype='step',
             label='FISH', normed=True, color='gray', lw=1)
     ax.text(0.5, 0.8, '{} - {}'.format(probes[l1][0], probes[l2][0]),
             transform=ax.transAxes)
@@ -78,10 +76,12 @@ def plot_distance_hists(ax, i, l1, l2):
         ax.spines[x].set_visible(False)
 
         
-def plot_all_hists(axes):
+def plot_all_hists(axes, plot_data_file):
 
-    for i, (l1, l2) in enumerate(combs):
-        plot_distance_hists(axes[i], i, l1, l2)
+    isd_distances, fish_distances = np.load(plot_data_file)
+    
+    for i, (l1, l2) in enumerate(combinations):
+        plot_distance_hists(axes[i], isd_distances[i], fish_distances[i], l1, l2)
 
 
 if False:
@@ -96,3 +96,17 @@ if False:
     path = os.path.expanduser('~/projects/ehic-paper/nmeth/supplementary_information/figures/nora_distance_histograms/')
     fig.savefig(path + '{}structures{}.svg'.format(n_structures, rep))
     fig.savefig(path + '{}structures{}.pdf'.format(n_structures, rep))
+
+
+if __name__ == "__main__":
+
+    import sys
+    from cPickle import dump
+
+    config_file = sys.argv[1]
+    fish_data_file = sys.argv[2]
+    output_file = sys.argv[3]
+    isd_distance_dists, fish_distance_hists = calculate_data(config_file, fish_data_file)
+
+    with open(output_file, "w") as opf:
+        dump((isd_distance_dists, fish_distance_hists), opf)
--- scripts/plots/nora2012/make_nora_verification_plots.py
+++ scripts/plots/nora2012/make_nora_verification_plots.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.expanduser('~/projects/ensemble_hic/scripts/plots/nora20
 fish_axes = [plt.subplot2grid(fig_shape, (i / 3, (i % 3) * 2), colspan=2)
              for i in range(7)]
 from distance_distributions import plot_all_hists
-plot_all_hists(fish_axes)
+plot_all_hists(fish_axes, "plot_data/distance_distributions.pickle")
 
 from differentiation import plot_before_hist, plot_after_hist
 before_ax = plt.subplot2grid(fig_shape, (3, 0), rowspan=2, colspan=3)
